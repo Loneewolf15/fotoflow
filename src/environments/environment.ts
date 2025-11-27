a937c07722f7d88e86ae2d2ef391fb6d008f4746
Lonewolf15 <loneewolf15@protonmail.com>
2025-11-27 13:56:48 -0800

    fixed build err

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,3 +5,4 @@ export const environment = {
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tdHJzeWZxd3dodWVmZG1uem5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNjM1MTcsImV4cCI6MjA3OTgzOTUxN30.BB09rss0U78mctKPj2YREXVplrjpY3rDl4dO79fLZf8'
   }
 };
+//david
